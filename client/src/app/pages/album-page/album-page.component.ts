fcc222a6597e28938061b71c5bbdc6006bbbbc58
Jane Lai <63269197+janelai@users.noreply.github.com>
2024-02-29 12:35:10 -0800

    added album page design components

---
+++ client/src/app/pages/album-page/album-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArtistData } from '../../data/artist-data';
+import { TrackData } from '../../data/track-data';
+import { AlbumData } from '../../data/album-data';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
+@Component({
+  selector: 'app-album-page',
+  templateUrl: './album-page.component.html',
+  styleUrls: ['./album-page.component.css']
+})
+export class AlbumPageComponent implements OnInit {
+	albumId:string;
+	album:AlbumData;
+	tracks:TrackData[];
+
+
+  constructor(private route: ActivatedRoute, private spotify: SpotifyService) { }
+
+  ngOnInit() {
+  	this.albumId = this.route.snapshot.paramMap.get('id');
+  	//TODO: inject spotifyService and use it to get the album data and the tracks for the album
+    this.spotify.getAlbum(this.albumId).then((albm)=>{
+      this.album = albm;
+    });
+
+    this.spotify.getTracksForAlbum(this.albumId).then((trks)=>{
+      this.tracks = trks;
+    });
+  }
+
+}
